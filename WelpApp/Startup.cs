356be9679c5dbc529fd9c3a52e91f43d276c7e68
Denise Williams <williamsde@outlook.com>
2016-08-04 13:17:00 -0400

    Add project files.

---
+++ WelpApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WelpApp.Startup))]
+namespace WelpApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
