afc9da30978daf91e749c009992d97d56dd8a8cd
Denise Williams <williamsde@outlook.com>
2016-08-05 11:39:31 -0400

    Authorize users in filter.config

--- WelpApp/App_Start/FilterConfig.cs
+++ WelpApp/App_Start/FilterConfig.cs
@@ -8,6 +8,8 @@ namespace WelpApp
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new System.Web.Mvc.AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
